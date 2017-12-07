ef45618d225e57e89851b7cff9dbfe0bc9ec4364
Jakub Czyż <j.czyz@omegaitc.pl>
2017-12-07 10:34:56 +0100

    init

---
+++ GeneticTSP/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GeneticTSP
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        void AppStartup(object sender, StartupEventArgs e)
+        {
+            Views.MainView window = new Views.MainView();
+            window.Show();
+        }
+
+        void ApplicationDispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show("Wystąpił nieobsługiwany wyjątek: " + e.Exception.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled = true;
+        }
+    }
+}
---
+++ GeneticTSP/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace GeneticTSP.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ExitButtonCommand { get; set; }
+
+        public MainViewModel()
+        {
+            ExitButtonCommand = new RelayCommand(ExitButtonClick);
+        }
+
+        private void ExitButtonClick(object v)
+        {
+            Environment.Exit(0);
+        }
+
+        public void NotifyPropertyChanged(string s)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(s));
+        }
+    }
+}
